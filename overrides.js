9d0ab1442a1b73ebb50bae90019b8a0343a61d30
Rob Loach <robloach@gmail.com>
2020-01-11 20:16:02 -0500

    Update overrides.js

--- overrides.js
+++ overrides.js
@@ -4,4 +4,5 @@ module.exports = function (title) {
 		.replace('wheelman', 'Wheelman')
 		.replace('never dies', 'Never Dies')
 		.replace('Saga Frontier', 'SaGa Frontier')
+		.replace('Defcon 5 - Piece Has a Price', 'Defcon 5 - Peace Has a Price')
 }
