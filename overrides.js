fd3ea3130a103bb8e8d67565fa9d893a234907b2
Justme488 <justme488@gmail.com>
2020-01-12 00:59:20 -0500

    Update overrides.js

--- overrides.js
+++ overrides.js
@@ -11,4 +11,8 @@ module.exports = function (title) {
 		.replace('Arc the Lad Collection (USA) (Disc 4 of 6)', 'Arc the Lad III (USA) (Disc 2 of 2)')
 		.replace('Arc the Lad Collection (USA) (Disc 5 of 6)', 'Arc the Lad Collection: Arc Arena: Monster Tournament (USA)')
 		.replace('Arc the Lad Collection (USA) (Disc 6 of 6)', 'Arc the Lad Collection: The Making of Arc the Lad (USA)')
+		.replace('Capcom Generations (Europe) (Disc 1 of 4)', 'Capcom Generations (Europe) (Disc 1) (Wings of Destiny)')
+		.replace('Capcom Generations (Europe) (Disc 2 of 4)', 'Capcom Generations (Europe) (Disc 2) (Chronicles of Arthur)')
+		.replace('Capcom Generations (Europe) (Disc 3 of 4)', 'Capcom Generations (Europe) (Disc 3) (The First Generation)')
+		.replace('Capcom Generations (Europe) (Disc 4 of 4)', 'Capcom Generations (Europe) (Disc 4) (Blazing Guns)')
 }
