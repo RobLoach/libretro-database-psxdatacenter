1564ee01c61f773a29b1b7c431d4a28dadac4cc1
Justme488 <justme488@gmail.com>
2020-01-11 23:10:31 -0500

    Update overrides.js

--- overrides.js
+++ overrides.js
@@ -5,4 +5,10 @@ module.exports = function (title) {
 		.replace('never dies', 'Never Dies')
 		.replace('Saga Frontier', 'SaGa Frontier')
 		.replace('Defcon 5 - Piece Has a Price', 'Defcon 5 - Peace Has a Price')
+		.replace('Arc the Lad Collection (USA) (Disc 1 of 6)', 'Arc the Lad Collection: Arc the Lad (USA)')
+		.replace('Arc the Lad Collection (USA) (Disc 2 of 6)', 'Arc the Lad Collection: Arc the Lad II (USA)')
+		.replace('Arc the Lad Collection (USA) (Disc 3 of 6)', 'Arc the Lad III (USA) (Disc 1 of 2)')
+		.replace('Arc the Lad Collection (USA) (Disc 4 of 6)', 'Arc the Lad III (USA) (Disc 2 of 2)')
+		.replace('Arc the Lad Collection (USA) (Disc 5 of 6)', 'Arc the Lad Collection: Arc Arena: Monster Tournament (USA)')
+		.replace('Arc the Lad Collection (USA) (Disc 6 of 6)', 'Arc the Lad Collection: The Making of Arc the Lad (USA)')
 }
